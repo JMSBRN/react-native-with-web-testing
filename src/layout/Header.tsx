d396c4fa34047c762ec1db29f4b769c37009a7cc
JMSBRN <zakhavai@gmail.com>
2024-05-08 21:07:53 +0300

    fix: testing tem logic( not end)

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -4,16 +4,19 @@ import {color} from '../styles/styles';
 import useHideElement from '../hooks/useHideElement';
 import NavLinks from '../components/NavLinks';
 import MainButton from '../components/MainButton';
-import {useNavigation} from '@react-navigation/native';
+import {Theme, useNavigation, useTheme} from '@react-navigation/native';
 function Header({
   setModalVisible,
+  handleToggleTheme,
 }: {
   setModalVisible?: React.Dispatch<React.SetStateAction<boolean>>;
+  handleToggleTheme: () => void;
 }) {
   const isNotMobileSize = useHideElement(650);
   const navigation = useNavigation();
   const platform = Platform.OS;
   const isDescTopSize = isNotMobileSize && platform !== 'android';
+  const theme = useTheme();
   const handleToggleModal = () => {
     if (setModalVisible) {
       setModalVisible(true);
@@ -24,24 +27,31 @@ function Header({
   };
 
   return (
-    <View style={styles.headerWrapper}>
-      <View style={styles.container}>
+    <View style={styles().headerWrapper}>
+      <View style={styles(theme).container}>
         {!isNotMobileSize && (
-          <MainButton
-            onPress={handleToggleModal}
-            bgColorProp={color.secondary}
-            title="Menu"
-          />
+          <>
+            <MainButton
+              onPress={handleToggleModal}
+              bgColorProp={color.secondary}
+              title="Menu"
+            />
+            <MainButton
+              onPress={handleToggleTheme}
+              bgColorProp={color.blue}
+              title="theme"
+            />
+          </>
         )}
         {isDescTopSize && (
           <>
-            <View style={styles.logo}>
-              <Text style={styles.logoTitle}>Logo</Text>
+            <View style={styles().logo}>
+              <Text style={styles().logoTitle}>Logo</Text>
             </View>
             <NavLinks />
           </>
         )}
-        <View style={styles.authBlock}>
+        <View style={styles().authBlock}>
           <MainButton
             onPress={() => handleNavigate('Login' as never)}
             title="Log In"
@@ -58,45 +68,46 @@ function Header({
 
 export default Header;
 
-const styles = StyleSheet.create({
-  headerWrapper: {
-    width: '100%',
-    height: 64,
-    backgroundColor: color.primary,
-    justifyContent: 'center',
-  },
-  container: {
-    paddingHorizontal: 20,
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-  },
-  logo: {
-    width: 50,
-    height: 50,
-    backgroundColor: color.gray,
-    borderRadius: 50,
-    overflow: 'hidden',
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  logoTitle: {
-    color: color.light,
-  },
-  flatListContainer: {
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-    alignItems: 'center',
-  },
-  link: {
-    marginHorizontal: 13,
-    fontSize: 16,
-  },
-  authBlock: {
-    flexDirection: 'row',
-    columnGap: 20,
-  },
-});
+const styles = (theme?: Theme) =>
+  StyleSheet.create({
+    headerWrapper: {
+      width: '100%',
+      height: 64,
+      backgroundColor: theme?.colors.background,
+      justifyContent: 'center',
+    },
+    container: {
+      paddingHorizontal: 20,
+      display: 'flex',
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      alignItems: 'center',
+    },
+    logo: {
+      width: 50,
+      height: 50,
+      backgroundColor: color.gray,
+      borderRadius: 50,
+      overflow: 'hidden',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    logoTitle: {
+      color: color.light,
+    },
+    flatListContainer: {
+      display: 'flex',
+      flexDirection: 'row',
+      justifyContent: 'space-around',
+      alignItems: 'center',
+    },
+    link: {
+      marginHorizontal: 13,
+      fontSize: 16,
+    },
+    authBlock: {
+      flexDirection: 'row',
+      columnGap: 20,
+    },
+  });
